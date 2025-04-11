385f4e49195ef23ff67e58708686bc65b06b9571
Matt <matiasobando@elpuig.xeill.net>
2025-04-11 18:43:46 +0200

    Trabajo zona1 piercing.php

--- includes/templates/piercing.php
+++ includes/templates/piercing.php
@@ -3,7 +3,7 @@
 
       <!-- Section Title -->
       <div class="container section-title" data-aos="fade-up">
-        <h2>Pricing</h2>
+        <h2>Provando cosas en git con samuel</h2>
         <p>Necessitatibus eius consequatur ex aliquid fuga eum quidem sint consectetur velit</p>
       </div><!-- End Section Title -->
 
