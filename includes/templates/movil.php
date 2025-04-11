718894f2825b32a0cfefb4c5b0216b0806199b20
york247 <samuelmataarias@elpuig.xeill.net>
2025-04-11 19:04:08 +0200

    cambiando movil.php

--- includes/templates/movil.php
+++ includes/templates/movil.php
@@ -5,7 +5,7 @@
         <div class="feature-item text-end mb-5" data-aos="fade-right" data-aos-delay="200">
           <div class="d-flex align-items-center justify-content-end gap-4">
             <div class="feature-content">
-              <h3>Use On Any Device</h3>
+              <h3>Usa un p devise</h3>
               <p>Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia.</p>
             </div>
             <div class="feature-icon flex-shrink-0">
